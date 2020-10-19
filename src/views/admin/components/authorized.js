360e6901a04ef899c8dae89fb8ffab3bd77c3b0f
Ismaeel Akram <ismaeelakram01@gmail.com>
2020-10-18 23:46:33 -0400

    Add another message for users who aren't logged in

--- src/views/admin/components/authorized.js
+++ src/views/admin/components/authorized.js
@@ -5,23 +5,24 @@ import {Container, Heading, Subheading} from '../style'
 export default function Authorized(props){
     const firebase = useFirebase();
     let currentUser = firebase.auth().currentUser;
-    if(!currentUser === null){
-        if(currentUser.email === 'korlov9026@bths.edu' || currentUser.email === 'mbilik0726@bths.edu' || currentUser.email === 'iakram2586@bths.edu'){
-            return props.children;
-        }else {
-            return (
-                <Container>
-                      <Heading>Oops!</Heading>
-                      <Subheading>You aren't allowed to see this page.</Subheading>
-                  </Container>
-            );
-        }
-    } else {
-        return (
-            <Container>
-                  <Heading>Oops!</Heading>
-                  <Subheading>You aren't allowed to see this page.</Subheading>
-              </Container>
-        );
-    }
+    let unauthorized = (<Container>
+        <Heading>Oops!</Heading>
+        <Subheading>You aren't allowed to see this page.</Subheading>
+    </Container>);
+    let notLoggedIn = (<Container>
+        <Heading>Oops!</Heading>
+        <Subheading>You aren't logged in.</Subheading>
+    </Container>);
+    if(currentUser === null){ return notLoggedIn; }
+
+    if(
+        currentUser.email === 'korlov9026@bths.edu' || 
+        currentUser.email === 'mbilik0726@bths.edu' || 
+        currentUser.email === 'iakram2586@bths.edu'
+    ){
+        return props.children;
+    } 
+
+    return unauthorized;
+
 }
