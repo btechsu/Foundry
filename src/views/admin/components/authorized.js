2d73e65e727b060d1e40be339dd1838de4f28b96
Ismaeel Akram <ismaeelakram01@gmail.com>
2020-10-18 23:24:27 -0400

    fix unauthed bug

--- src/views/admin/components/authorized.js
+++ src/views/admin/components/authorized.js
@@ -5,9 +5,16 @@ import {Container, Heading, Subheading} from '../style'
 export default function Authorized(props){
     const firebase = useFirebase();
     let currentUser = firebase.auth().currentUser;
-    if(!currentUser == null){
-        if(currentUser.email === 'korlov9026@bths.edu' || currentUser.email === 'mbilik0726@bths.edu' || currentUser.email == 'iakram2586@bths.edu'){
+    if(!currentUser === null){
+        if(currentUser.email === 'korlov9026@bths.edu' || currentUser.email === 'mbilik0726@bths.edu' || currentUser.email === 'iakram2586@bths.edu'){
             return props.children;
+        }else {
+            return (
+                <Container>
+                      <Heading>Oops!</Heading>
+                      <Subheading>You aren't allowed to see this page.</Subheading>
+                  </Container>
+            );
         }
     } else {
         return (
